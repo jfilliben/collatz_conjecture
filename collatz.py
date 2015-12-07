076949e7eaf327671d9630dd7e646d7525d5d07c
Jeremy Filliben <jfilliben@gmail.com>
2015-12-07 14:41:23 -0500

    change default repetitions to 1000
    change default repetitions to 1000

--- collatz.py
+++ collatz.py
@@ -8,6 +8,7 @@ import sys
 import time
 
 random.seed()
+
 user_input = raw_input("Enter lower range [1]: ")
 if user_input.isdigit():
     rand_min = int(user_input)
@@ -20,11 +21,12 @@ else:
     rand_max = 10000
 if rand_max < rand_min:
     sys.exit("Error, max cannot be less than min")
-user_input = raw_input("How many repetitions? [100]: ")
+user_input = raw_input("How many repetitions? [1000]: ")
 if user_input.isdigit():
     num_repetitions = int(user_input)
 else:
-    num_repetitions = 10000
+    num_repetitions = 1000
+
 highest = (1,1,1)
 print "Original_N     Max_N               Num_Steps"
 for x in range(1,num_repetitions):
@@ -43,6 +45,7 @@ for x in range(1,num_repetitions):
     if step_count > highest[2]:
         highest = (original_n, max, step_count)
     print "{0:<14} {1:<16} {2:6}".format(original_n, max, step_count)
+
 print "Most Steps = {}".format(highest[2])
 print "N = {}".format(highest[0])
 print "Highest intermediate value for N = {} was {}".format(highest[0],
